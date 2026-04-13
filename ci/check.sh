8d3012ce6661e4cba04c25dacb4e24b754f98f6e
Mikhail T. <github-assail-2026@virtual-estates.net>
2026-04-13 14:41:53 -0400

    CI: fix FreeBSD sudo path, add sudo for macOS cpanm

--- ci/check.sh
+++ ci/check.sh
@@ -2,7 +2,11 @@
 set -ex
 
 # Use the same perl as spamassassin itself
-SA=$(which spamassassin)
-PERL=$(head -1 "$SA" | sed 's/^#![ ]*//' | awk '{print $1}')
+SA=$(which spamassassin 2>/dev/null || true)
+if [ -n "$SA" ]; then
+	PERL=$(head -1 "$SA" | sed 's/^#![ ]*//' | awk '{print $1}')
+else
+	PERL=perl
+fi
 
 $PERL -c AI/AI.pm
